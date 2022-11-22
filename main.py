aaef034611907a4ec38bb6351375b664a3c62888
Nuno-Ferreira <nc766@live.mdx.ac.uk>
2022-11-22 14:22:29 +0000

    added ellipse contour

--- main.py
+++ main.py
@@ -11,29 +11,34 @@ kernel = np.ones((5, 5), np.uint8)
 img_close = cv2.morphologyEx(img_th, cv2.MORPH_OPEN, kernel)      # morphology correction
 img_canny = cv2.Canny(img_close, 50, 100)                          # edge detection
 contours, hierarchy = cv2.findContours(img_close, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)   # extract contours on binarised image, not on canny
-
+"""
 cv2.drawContours(img_colour, contours, -1, (0, 255, 0), 1)         # paint contours on top of original coloured mage
 cv2.imshow('picture', img_colour)
 cv2.imshow('edges', img_canny)
 key = cv2.waitKey(0)
 cv2.destroyAllWindows()
-
+"""
 print(len(contours))
 
 
 imgc = cv2.imread('D:/GitHub/recognise_uno_cards/images/b0.jpg')    # open the saved image in colour 
 for i, c in enumerate(contours):         # loop through all the found contours
     print(i, ':', hierarchy[0, i])          # display contour hierarchy
-    #print('length: ', len(c))               # display numbr of points in contour c
+    print('length: ', len(c))               # display numbr of points in contour c
     perimeter = cv2.arcLength(c, True)     # perimeter of contour c (curved length)
-    #print('perimeter: ', perimeter)               
+    print('perimeter: ', perimeter)               
     epsilon = 0.02*perimeter    # parameter of polygon approximation: smaller values provide more vertices
     vertex_approx = len(cv2.approxPolyDP(c, epsilon, True))     # approximate with polygon
-    #print('approx corners: ', vertex_approx, '\n')                    # number of vertices
+    print('approx corners: ', vertex_approx, '\n')                    # number of vertices
+    ellipse = cv2.fitEllipse(c)
+    (xy,xc),(d1,d2),angle = ellipse
+    axes_ratio = round(d1/d2, 3)
+    print('axes ratio: ', axes_ratio, '\n')
     cv2.drawContours(imgc, [c], 0, (0, 255, 0), 3)   # paint contour c
     cv2.putText(imgc, str(i), (c[0, 0, 0]+20, c[0, 0, 1]+30), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255))    # identify contour c
     [x,y,w,h] = cv2.boundingRect(c)
-    cv2.rectangle(imgc, (x,y), (x+w,y+h), (255, 0, 0), 2)
+    #cv2.rectangle(imgc, (x,y), (x+w,y+h), (255, 0, 0), 2)
+    cv2.ellipse(imgc, ellipse, (255, 0, 0), 1)
 cv2.namedWindow('picture', cv2.WINDOW_NORMAL)
 cv2.imshow('picture',imgc)
 key = cv2.waitKey(0)
