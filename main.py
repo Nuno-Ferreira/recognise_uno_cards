246588ddc49eaae7caee751502208de9adaed738
Nuno-Ferreira <nc766@live.mdx.ac.uk>
2022-11-24 19:13:51 +0000

    changed the file input

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import cv2
 import numpy as np
+import os
 
+folder_path = r'D:/GitHub/recognise_uno_cards/images/'
 
-img_colour = cv2.imread('D:/GitHub/recognise_uno_cards/images/b0.jpg')  # open the saved image in colour
+card = 'b0'
+
+img_colour = cv2.imread(folder_path + card + '.jpg')  # open the saved image in colour
 
 img = cv2.cvtColor(img_colour, cv2.COLOR_BGR2GRAY)   # convert to B/W
-img_sm = cv2.blur(img, (1, 1))         # smoothing
-thr_value, img_th = cv2.threshold(img_sm, 0, 255, cv2.THRESH_OTSU)   # binarisation
+#img_sm = cv2.blur(img, (1, 1))         # smoothing
+thr_value, img_th = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)   # binarisation
 kernel = np.ones((5, 5), np.uint8)
 img_close = cv2.morphologyEx(img_th, cv2.MORPH_OPEN, kernel)      # morphology correction
 img_canny = cv2.Canny(img_close, 50, 100)                          # edge detection
@@ -21,7 +25,7 @@ cv2.destroyAllWindows()
 print(len(contours))
 
 data = []
-imgc = cv2.imread('D:/GitHub/recognise_uno_cards/images/b0.jpg')    # open the saved image in colour 
+imgc = cv2.imread(folder_path + card + '.jpg')    # open the saved image in colour 
 for i, c in enumerate(contours):         # loop through all the found contours
     print(i, ':', hierarchy[0, i])          # display contour hierarchy
     print('length: ', len(c))               # display numbr of points in contour c
@@ -31,13 +35,13 @@ for i, c in enumerate(contours):         # loop through all the found contours
     vertex_approx = len(cv2.approxPolyDP(c, epsilon, True))     # approximate with polygon
     print('approx corners: ', vertex_approx, '\n')                    # number of vertices
     ellipse = cv2.fitEllipse(c)
-    (xy,xc),(d1,d2),angle = ellipse
+    (xc,yc),(d1,d2),angle = ellipse
     axes_ratio = round(d1/d2, 3)
     print('axes ratio: ', axes_ratio, '\n')
-    cv2.drawContours(imgc, [c], 0, (0, 255, 0), 3)   # paint contour c
-    cv2.putText(imgc, str(i), (c[0, 0, 0]+20, c[0, 0, 1]+30), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255))    # identify contour c
+    cv2.drawContours(imgc, [c], 0, (0, 255, 0), 2)   # paint contour c
+   # cv2.putText(imgc, str(i), (c[0, 0, 0]+20, c[0, 0, 1]+30), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255))    # identify contour c
     [x,y,w,h] = cv2.boundingRect(c)
-    #cv2.rectangle(imgc, (x,y), (x+w,y+h), (255, 0, 0), 2)
+   # cv2.rectangle(imgc, (x,y), (x+w,y+h), (255, 0, 0), 2)
     cv2.ellipse(imgc, ellipse, (255, 0, 0), 1)
 
     #sample = [] #add the features here so that I can use data to extract all the features to do machine learning 
