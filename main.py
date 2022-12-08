ccaf99973f3f519ee1490a280dca225b6dde251b
Nuno-Ferreira <nc766@live.mdx.ac.uk>
2022-12-08 20:03:45 +0000

    singled out the right contours

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Users\nunom\AppData\Local\Programs\
 
 folder_path = r'D:/GitHub/recognise_uno_cards/images/'
 
-card = 'b5'
+card = 'b0'
 card_colors = ['b', 'g', 'y', 'r']
 #implement for loop to go through all the images, use it to go through colors and numbers | implement wait time if needed to change cards
 #for c in card_colors:
@@ -28,26 +28,29 @@ contours, hierarchy = cv2.findContours(thresholdImage, cv2.RETR_TREE, cv2.CHAIN_
 data = []
 
 for i, c in enumerate(contours):         # loop through all the found contours
-    print(i, ':', hierarchy[0, i])          # display contour hierarchy
-    print('length: ', len(c))               # display numbr of points in contour c
-    perimeter = cv2.arcLength(c, True)     # perimeter of contour c (curved length)
-    print('perimeter: ', perimeter)               
-    epsilon = 0.02*perimeter    # parameter of polygon approximation: smaller values provide more vertices
-    vertex_approx = len(cv2.approxPolyDP(c, epsilon, True))     # approximate with polygon
-    print('approx corners: ', vertex_approx, '\n')                    # number of vertices
-    if len(contours[i]) >= 5:
-      ellipse = cv2.fitEllipse(c)
-    (xc,yc),(d1,d2),angle = ellipse
+    if hierarchy[0,i,3] != -1 and hierarchy[0,i,3] != 0:
+      print(i, ':', hierarchy[0, i])          # display contour hierarchy
+      print('length: ', len(c))               # display numbr of points in contour c
+      perimeter = cv2.arcLength(c, True)     # perimeter of contour c (curved length)
+      print('perimeter: ', perimeter)               
+      epsilon = 0.02*perimeter    # parameter of polygon approximation: smaller values provide more vertices
+      vertex_approx = len(cv2.approxPolyDP(c, epsilon, True))     # approximate with polygon
+      print('approx corners: ', vertex_approx, '\n')                    # number of vertices
+      if len(contours[i]) >= 5:
+        ellipse = cv2.fitEllipse(c)
+      (xc,yc),(d1,d2),angle = ellipse
 
-    axes_ratio = round(d1/d2, 3)
-    print('axes ratio: ', axes_ratio, '\n')
-    cv2.drawContours(img_colour, [c], 0, (0, 255, 0), 2)   # paint contour c
-    cv2.putText(img_colour, str(i), (c[0, 0, 0]+20, c[0, 0, 1]+30), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255))    # identify contour c
-    #[x,y,w,h] = cv2.boundingRect(c)
-   # cv2.rectangle(img_colour, (x,y), (x+w,y+h), (255, 0, 0), 2)
-    cv2.ellipse(img_colour, ellipse, (255, 0, 0), 2)
+      axes_ratio = round(d1/d2, 3)
+      print('axes ratio: ', axes_ratio, '\n')
+      cv2.drawContours(img_colour, [c], 0, (0, 255, 0), 2)   # paint contour c
+      cv2.putText(img_colour, str(i), (c[0, 0, 0]+20, c[0, 0, 1]+30), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255))    # identify contour c
+      #[x,y,w,h] = cv2.boundingRect(c)
+    # cv2.rectangle(img_colour, (x,y), (x+w,y+h), (255, 0, 0), 2)
+      cv2.ellipse(img_colour, ellipse, (255, 0, 0), 2)
 
     # add a function/if statement to get rid of all the other ellipses
+    #if hierarchy[3] != -1:
+
 
     
     #sample = [] #add the features here so that I can use data to extract all the features to do machine learning 
@@ -60,7 +63,7 @@ for i, c in enumerate(contours):         # loop through all the found contours
 cv2.namedWindow('picture', cv2.WINDOW_NORMAL)
 cv2.imshow('picture',img_colour)
 #<cv2.imshow('gray', img)
-cv2.imshow('Image', thresholdImage)
+#cv2.imshow('Image', thresholdImage)
 key = cv2.waitKey(0)
 cv2.destroyAllWindows()
 
