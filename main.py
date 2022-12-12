c6dea16e9921b1af9251cd27fa3cbf181704d776
Nuno-Ferreira <nc766@live.mdx.ac.uk>
2022-12-12 10:00:06 +0000

    added machine learning imports and code

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 import cv2
 import numpy as np
 import os
+import matplotlib.pyplot as plt
+#from tensorflow.keras.models import load_model 
+from sklearn import datasets
+from sklearn.datasets import load_digits
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split, cross_val_score
+from keras.datasets import mnist
 
+
+
+"""
+model = load_model('filename')
+
+def prediciton(image, model):
+  img = cv2.resixe(image, (28, 28))
+  img = img / 255
+  img = img.reshape(1, 28, 28, 1)
+  predict = model.predict(img)
+  prob = np.amax(predict)
+  class_index = model.predict_classes(img)
+  result = class_index[0]
+  if prob < 0.75:
+    result = 0
+    prob = 0
+  return result, prob
+"""
+# then add the text and the prediction in the for loop
 
 # Folder path variable to add the card
 folder_path = r'D:/GitHub/recognise_uno_cards/images/'
@@ -11,16 +38,17 @@ card_colors = ['b', 'g', 'y', 'r']
 # yellow is the problematic colour since it doesn't show up in B/W card | it also has a lot of shadows in numbers
 
 for c in range(10):
-  card = card_colors[0] + str(c)    # is used to combine the number and the color letter in a string
+  card = card_colors[1] + str(c)    # is used to combine the number and the color letter in a string
   img_colour = cv2.imread(folder_path + card + '.jpg')  # open the saved image in colour
   img = cv2.cvtColor(img_colour, cv2.COLOR_BGR2GRAY)   # convert to B/W
-  #img_sm = cv2.blur(img, (1, 1))         # smoothing
-  thr_value, img_th = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)   # binarisation
-  kernel = np.ones((5, 5), np.uint8)
-  img_close = cv2.morphologyEx(img_th, cv2.MORPH_OPEN, kernel)      # morphology correction
-  img_canny = cv2.Canny(img_close, 50, 100)                          # edge detection
   estimatedThreshold, thresholdImage=cv2.threshold(img,160,255,cv2.THRESH_BINARY)
   contours, hierarchy = cv2.findContours(thresholdImage, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)   # extract contours on binarised image, not on canny
+
+  #img_sm = cv2.blur(img, (1, 1))         # smoothing
+  #thr_value, img_th = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)   # binarisation
+  #kernel = np.ones((5, 5), np.uint8)
+  #img_close = cv2.morphologyEx(img_th, cv2.MORPH_OPEN, kernel)      # morphology correction
+  #img_canny = cv2.Canny(img_close, 50, 100)                          # edge detection
 
   #data = []
 
@@ -33,8 +61,13 @@ for c in range(10):
         epsilon = 0.02*perimeter    # parameter of polygon approximation: smaller values provide more vertices
         vertex_approx = len(cv2.approxPolyDP(c, epsilon, True))     # approximate with polygon
         print('approx corners: ', vertex_approx, '\n')                    # number of vertices
+        area = cv2.contourArea(c)
+        print('area: ', area, '\n')
         if len(contours[i]) >= 5:
           ellipse = cv2.fitEllipse(c)
+        (center, axes, orientation) = ellipse
+        majoraxis_length = max(axes)
+        minoraxis_length = min(axes)
         (xc,yc),(d1,d2),angle = ellipse
         axes_ratio = round(d1/d2, 3)
         print('axes ratio: ', axes_ratio, '\n')
@@ -47,14 +80,27 @@ for c in range(10):
 
       # add an if statement to only add the contour that is biggest between the number in center and on the sides
 
-
+      # I can crop the image by the biggest contour and then add a feature to see if it has any children
       
       #sample = [] #add the features here so that I can use data to extract all the features to do machine learning 
       #data.append(sample)
+  X = numbers.data
+  y = numbers.target
+  corners = vertex_approx
+  shape_complexity = area/perimeter
+  relative_length = minoraxis_length/majoraxis_length
+  features = [corners, shape_complexity, axes_ratio, relative_length]
+
+  filename = card + '.jpg'
+  colour_label = filename[0]
+  number_label = int(filename[1])
+
+  np.concatenate(X, features)
+  np.concatenate(y, number_label)
+
 
   cv2.namedWindow('picture', cv2.WINDOW_NORMAL)
   cv2.imshow('picture',img_colour)
-  #<cv2.imshow('gray', img)
   cv2.imshow('Image', thresholdImage)
   key = cv2.waitKey(0)
   cv2.destroyAllWindows()
